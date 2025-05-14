bd4d3837ec4ad772e49b5a80f3d591f4041dee4a
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2025-05-14 20:57:04 -0300

    feat: adição de menu de bottom navigation no app, realizado já com o material design 3.

--- app/src/main/java/com/example/littlelemon/MainActivity.kt
+++ app/src/main/java/com/example/littlelemon/MainActivity.kt
@@ -8,10 +8,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -34,8 +42,11 @@ private fun AppScreen() {
     Scaffold (
         topBar = {
             TopAppBar()
+        },
+        bottomBar = {
+            MyBottonNavigation(navController = rememberNavController())
         }
-    ){
+     ){
         Box( modifier = Modifier
             .fillMaxSize()
             .padding(it)) {
@@ -62,90 +73,46 @@ fun MyNavigation (){
     }
 }
 
+@Composable
+fun MyBottonNavigation(navController: NavController) {
 
-//@Composable
-//fun MenuContent(paddingValues: PaddingValues) {
-//    Surface(modifier = Modifier.padding(paddingValues)) {
-//        val menuPadding = 8.dp
-//        val configuration = LocalConfiguration.current
-//        when (configuration.orientation) {
-//            ORIENTATION_LANDSCAPE -> {
-//                Column {
-//                    Row(modifier = Modifier.weight(0.5f)) {
-//                        Text(
-//                            "Appetizers",
-//                            modifier = Modifier
-//                                .weight(0.25f)
-//                                .fillMaxHeight()
-//                                .background(Purple80)
-//                                .padding(menuPadding)
-//                        )
-//                        Text(
-//                            "Salads",
-//                            modifier = Modifier
-//                                .weight(0.25f)
-//                                .fillMaxHeight()
-//                                .padding(menuPadding)
-//                        )
-//                    }
-//                    Row(modifier = Modifier.weight(0.5f)) {
-//                        Text(
-//                            "Drinks",
-//                            modifier = Modifier
-//                                .weight(0.25f)
-//                                .fillMaxHeight()
-//                                .background(Pink80)
-//                                .padding(menuPadding)
-//                        )
-//                        Text(
-//                            "Mains",
-//                            modifier = Modifier
-//                                .weight(0.25f)
-//                                .fillMaxHeight()
-//                                .background(PurpleGrey80)
-//                                .padding(menuPadding)
-//                        )
-//                    }
-//                }
-//            }
-//            else -> {
-//                Column {
-//                    Text(
-//                        "Appetizers",
-//                        modifier = Modifier
-//                            .weight(0.25f)
-//                            .background(Purple80)
-//                            .padding(menuPadding)
-//                            .fillMaxWidth()
-//                    )
-//                    Text(
-//                        "Salads",
-//                        modifier = Modifier
-//                            .weight(0.25f)
-//                            .padding(menuPadding)
-//                            .fillMaxWidth()
-//                    )
-//                    Text(
-//                        "Drinks",
-//                        modifier = Modifier
-//                            .weight(0.25f)
-//                            .background(Pink80)
-//                            .padding(menuPadding)
-//                            .fillMaxWidth()
-//                    )
-//                    Text(
-//                        "Mains",
-//                        modifier = Modifier
-//                            .weight(0.25f)
-//                            .background(PurpleGrey80)
-//                            .padding(menuPadding)
-//                            .fillMaxWidth()
-//                    )
-//                }
-//            }
-//        }
-//    }
-//}
+    val destinationList = listOf(
+        Menu,
+        Home,
+        Location
+    )
+
+    val selectIndex = rememberSaveable {
+        mutableStateOf(0)
+    }
+
+    NavigationBar {
+        destinationList.forEachIndexed { index, destination ->
+            NavigationBarItem(
+                label = { Text(destination.title) },
+                icon = {
+                    Icon(
+                        painter = painterResource(id = destination.icon),
+                        contentDescription = destination.title
+                    )
+                },
+                selected = index == selectIndex.value,
+                onClick = {
+                    selectIndex.value = index
+                    navController.navigate(destination.route) {
+                        popUpTo(navController.graph.startDestinationId) {
+                            saveState = true
+                        }
+
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                },
+            )
+        }
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
